58d98732fbdada93c1706076482a30f572c7197c
Pavel Beloborodov <boocmp@yandex-team.ru>
2020-10-28 13:26:24 +0700

    Sooqa

--- sw.js
+++ sw.js
@@ -1,10 +1,2 @@
-// A no-op fetch handler.
-self.addEventListener('fetch', e => {});
-
-self.addEventListener('message', async function(event) {
-  if (event.data.command == 'setAppBadge') {
-    await navigator.setAppBadge(event.data.badge);
-    event.ports[0].postMessage('OK');
-  }
-  event.ports[0].postMessage('ERROR');
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
