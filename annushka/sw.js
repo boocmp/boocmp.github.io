74c78a38805de8b8edf52234c2738a5a5c8830fa
Pavel Beloborodov <boocmp@yandex-team.ru>
2021-09-09 17:08:45 +0700

    Annushka

--- annushka/sw.js
+++ annushka/sw.js
@@ -3,20 +3,21 @@ const urlsToCache = [
   'index.html',
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-      caches.open('basic-cache').then((cache) => {
-        return cache.addAll(urlsToCache);
-      }),
-  );
-
-  console.log("123");
+self.addEventListener('message' , m => {
   yandex.publicFeature.getWallpaperInfo(function(info) {
     if (chrome.runtime.lastError) {
       console.error("oops " + chrome.runtime.lastError.message);
     } else {
       console.log(info);
     }});
+});
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+      caches.open('basic-cache').then((cache) => {
+        return cache.addAll(urlsToCache);
+      }),
+  );
 });
 
 self.addEventListener('fetch', e => {
